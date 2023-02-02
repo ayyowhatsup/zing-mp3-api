73083b9cb41ce205f3b0f35273463a7000ec69ce
ayyowhatsup <95658537+ayyowhatsup@users.noreply.github.com>
2023-02-03 03:03:59 +0700

    config cors

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+var cors = require("cors")
+app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "Content-Type", "Authorization")
+    next()
+ })
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
